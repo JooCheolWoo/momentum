2c7a4be1349302aec87711039fb28ff047102944
Pobilover <tkfkdal@naver.com>
2023-06-14 20:49:55 +0900

    asdsadaa

--- build-docker.sh
+++ build-docker.sh
@@ -31,21 +31,23 @@ echo "---------- [Deploy Step - 7] : Remove Old Docker Image"
 docker rmi ${APP_NAME_OLD}:${server_version}
 
 #ssl
-docker cp ca_bundle.crt nginx-proxy:/etc/nginx/certs/ca_bundle.crt
-docker cp certificate.crt nginx-proxy:/etc/nginx/certs/certificate.crt
-docker cp private.key nginx-proxy:/etc/nginx/certs/private.key
+docker cp ./ca_bundle.crt nginx-proxy:/etc/nginx/certs/ca_bundle.crt
+docker cp ./certificate.crt nginx-proxy:/etc/nginx/certs/certificate.crt
+docker cp ./private.key nginx-proxy:/etc/nginx/certs/private.key
 
 # 8. Run new docker container
 echo "---------- [Deploy Step - 8] : Run New Docker Container"
-docker run -d -p ${PORT}:${PORT} \
-    -e VIRTUAL_HOST=www.todolist.o-r.kr,todolist.o-r.kr \
-    -e VIRTUAL_PORT=10900 \
-    -e HTTPS_METHOD=noredirect \
-    -v /etc/nginx/certs:/etc/nginx/certs \
-    --restart unless-stopped \
-    --link ${APP_NAME}:app \
-    --name ${APP_NAME} \
-    ${APP_NAME}:${server_version}
+# docker run -d -p ${PORT}:${PORT} \
+#     -e VIRTUAL_HOST=www.todolist.o-r.kr,todolist.o-r.kr \
+#     -e VIRTUAL_PORT=10900 \
+#     -e HTTPS_METHOD=noredirect \
+#     -v /etc/nginx/certs:/etc/nginx/certs \
+#     --restart unless-stopped \
+#     --link ${APP_NAME}:app \
+#     --name ${APP_NAME} \
+#     ${APP_NAME}:${server_version}
+
+docker run -e "VIRTUAL_HOST=todolist.o-r.kr,www.todolist.o-r.kr" -e "VIRTUAL_PORT=10900" -e "HTTPS_METHOD=noredirect" -v /path/to/certificate.crt:/etc/nginx/certs/certificate.crt -v /path/to/private.key:/etc/nginx/certs/private.key --link nginx-proxy --name ${APP_NAME} -d ${APP_NAME}:${server_version}
 
 
 # docker logs show
--- dockerfile
+++ dockerfile
@@ -10,11 +10,5 @@ COPY css ./css
 COPY img ./img
 COPY js ./js
 
-# 인증서 복사
-COPY ca_bundle.crt /etc/nginx/ssl/
-COPY certificate.crt /etc/nginx/ssl/
-COPY private.key /etc/nginx/ssl/private/
-
-
 # 포트 노출
 EXPOSE 10900
